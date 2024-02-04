bd42d767ea3f5b0828eccb0996bff6a7e7d4ccc7
David <davidgonsal@gmail.com>
2024-02-04 16:40:04 +0200

    Remove attribution section from page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,12 +17,6 @@
 		<Button buttonTxt="Instagram" />
 	</dev>
 </div>
-<div class="attribution">
-	Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
-		>Frontend Mentor</a
-	>. Coded by <a href="#">Your Name Here</a>.
-</div>
-
 <style>
 	.container {
 		background-color: var(--neutral-dark-grey);
